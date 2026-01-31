68ef8505c72a58ccebcc6bf345ccd76c321e95b4
Samikshya Shrestha <sami.college098@gmail.com>
2026-01-31 21:15:04 +0545

    removeitem

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ import { createItems } from "./items.js";
 
 let items = groceryItems;
 
-// Render App
+
 function render() {
   const app = document.getElementById("app");
   app.innerHTML = "";
@@ -12,11 +12,10 @@ function render() {
   app.appendChild(itemsElement);
 }
 
-// Initialize App
+
 render();
 
 
-// Edit Completed Function
 export function editCompleted(itemId) {
   items = items.map((item) => {
     if (item.id === itemId) {
@@ -25,4 +24,11 @@ export function editCompleted(itemId) {
     return item;
   });
   render();
+}
+
+
+export function removeItem(itemId) {
+  items = items.filter((item) => item.id !== itemId);
+  render();
+  setTimeout(() => alert("Item Deleted Successfully!"), 0);
 }
