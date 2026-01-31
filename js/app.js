f865d8bf207c9be1fcac7c455f6b6ec578f9801f
Samikshya Shrestha <sami.college098@gmail.com>
2026-01-31 21:03:42 +0545

    app-js

---
+++ js/app.js
@@ -0,0 +1,16 @@
+import { groceryItems } from "./data.js";
+import { createItems } from "./items.js";
+
+let items = groceryItems;
+
+// Render App
+function render() {
+  const app = document.getElementById("app");
+  app.innerHTML = "";
+
+  const itemsElement = createItems(items);
+  app.appendChild(itemsElement);
+}
+
+// Initialize App
+render();
