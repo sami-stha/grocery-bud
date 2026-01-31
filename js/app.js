ff0ecc32b774fbf6130a3bf60cc81c1af10085be
Samikshya Shrestha <sami.college098@gmail.com>
2026-01-31 21:28:44 +0545

    update

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 import { groceryItems } from "./data.js";
 import { createItems } from "./items.js";
+import { createForm } from "./form.js";
 
 let items = groceryItems;
 
@@ -8,10 +9,12 @@ function render() {
   const app = document.getElementById("app");
   app.innerHTML = "";
 
+  const formElement = createForm();
   const itemsElement = createItems(items);
+
+  app.appendChild(formElement);
   app.appendChild(itemsElement);
 }
-
 
 render();
 
@@ -31,4 +34,22 @@ export function removeItem(itemId) {
   items = items.filter((item) => item.id !== itemId);
   render();
   setTimeout(() => alert("Item Deleted Successfully!"), 0);
+}
+
+
+// Generate unique ID
+function generateId() {
+  return Date.now().toString(36) + Math.random().toString(36).substr(2);
+}
+
+// Add Item Function
+export function addItem(itemName) {
+  const newItem = {
+    name: itemName,
+    completed: false,
+    id: generateId(),
+  };
+  items = [...items, newItem];
+  render();
+  setTimeout(() => alert("Item Added Successfully!"), 0);
 }
